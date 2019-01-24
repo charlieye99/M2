1fbd5a28ae8cda90d7146e1f828b85801fcd60ea
Lina Jooeun Kim <linajkim@lawn-143-215-88-19.lawn.gatech.edu>
2019-01-24 18:06:02 -0500

    Edited Person5.java

--- src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -3,8 +3,8 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 5
  *  returns their name and a
- *  modified string 
- *  
+ *  modified string
+ *
  *  @author Bob
  *  @version 1.1
  */
@@ -30,16 +30,27 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
+
+		String modStr = "";
+
+		if (input == null) {
+			return null;
+		} else if (input.length() == 2) {
+			return input;
+		} else {
+
+			modStr = input.substring(2, input.length()) + input.substring(0, 2);
+
+			return modStr;
+		}
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
