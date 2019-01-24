f29057cfaf39d943af96fc538fd2e07d7effb2df
Saira <sairapoonnen@gmail.com>
2019-01-24 00:06:51 -0500

    Changes to Person1.java

--- src/main/java/edu/gatech/oad/antlab/person/Person1.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person1.java
@@ -3,9 +3,9 @@ package edu.gatech.oad.antlab.person;
 /**
  *  A simple class for person 1
  *  returns their name and a
- *  modified string 
- *  
- *  @author Bob
+ *  modified string
+ *
+ *  @author Saira
  *  @version 1.1
  */
 public class Person1 {
@@ -31,15 +31,24 @@ public class Person1 {
 	 */
 	private String calc(String input) {
 	  //Person 1 put your implementation here
-	  return null;
+
+		char c1 = input.charAt(0);
+		char c2 = input.chartAt(1);
+
+		String ret;
+
+		String s = input.substring(2);
+
+		ret = s + c1 + c2;
+	  return ret;
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
 	 *
 	 * @param input the varying string
-	 * @return the string representing the 
+	 * @return the string representing the
 	 *         object
 	 */
 	public String toString(String input) {
